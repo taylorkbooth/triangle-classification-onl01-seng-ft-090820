6e426435af844197ab87c2767c6fb430767d51ec
Taylor Booth <taylorkbooth@gmail.com>
2020-09-29 15:42:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,12 +12,10 @@ class Triangle
   def kind
     if @sides.any? {|s| s <= 0} or
        @sides[0]+ @sides[1] <= @sides[2]
-  # if   # x + y < z || z + y < x || x + z < y || x <= 0 || x == nil || y <=  0 || y == nil || z <=  0 || z == nil 
         raise TriangleError
      elsif x == y && y == z && x == z
       :equilateral
     elsif 
-      # (@sides[0] == @sides[1] && @sides[1] != @sides[2]) || (@sides[1] == @sides[2] && @sides[1] != @sides[0])
     (x == y && x !=z) || (y == z && x != z) || (z == x && x != y)
       :isosceles
     elsif x != y && y != z && x != z
