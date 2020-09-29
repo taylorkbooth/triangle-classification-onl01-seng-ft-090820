d7a46056ee429e8c557d7101286600d2989b8e8a
Taylor Booth <taylorkbooth@gmail.com>
2020-09-29 15:39:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,23 +3,27 @@ class Triangle
   attr_accessor :x, :y, :z, :equilateral, :isosceles, :scalene
 
   def initialize(x, y, z)
-    
-    
-    
-    # @equilateral = equilateral
-    # @isosceles = isosceles
-    # @scalene = scalene
+    @x = x 
+    @y = y 
+    @z = z
+    @sides = [x,y,z].sort 
+  end 
+
+  def kind
+    if @sides.any? {|s| s <= 0} or
+       @sides[0]+ @sides[1] <= @sides[2]
+  # if   # x + y < z || z + y < x || x + z < y || x <= 0 || x == nil || y <=  0 || y == nil || z <=  0 || z == nil 
+        raise TriangleError
+     elsif x == y && y == z && x == z
+      :equilateral
+    elsif 
+      # (@sides[0] == @sides[1] && @sides[1] != @sides[2]) || (@sides[1] == @sides[2] && @sides[1] != @sides[0])
+    (x == y && x !=z) || (y == z && x != z) || (z == x && x != y)
+      :isosceles
+    elsif x != y && y != z && x != z
+      :scalene
+    end 
   end
 
-
-
-
-
-
-
-
-
-
-
-
-end
+  class TriangleError < StandardError
+  end
